ca9634456bdd4471d22d64117990c78dcc6c2fd4
254Davidhashisoma <davidhashisoma95@gmail.com>
2021-04-11 10:01:14 +0300

    modification app setting

--- webinnar/settings.py
+++ webinnar/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from pathlib import Path
 
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -37,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'posts',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [

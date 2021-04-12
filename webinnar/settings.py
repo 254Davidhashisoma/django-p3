ef01a67380421b96f87edbb18632bfd656f8969f
254Davidhashisoma <davidhashisoma95@gmail.com>
2021-04-12 23:50:49 +0300

    deploying to heroku

--- webinnar/settings.py
+++ webinnar/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7k0+f$w!@b6izx$oy@sadzir-&7i02$z1ws-lo9(epnf_03ll3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
